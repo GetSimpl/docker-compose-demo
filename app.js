b2f94633356dbc4f50116bfb7ad3cfc3d1fa4a0b
Arun Saragadam <iarunsaragadam@gmail.com>
2017-05-03 17:36:42 +0530

    Added Dockerfile, docker-compose for a working demo

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
 
-  app.get('/', function (req, res) {
-      res.send('Hello World!')
-  })
+app.get('/', function (req, res) {
+    res.send('This is version no. ' + process.env.VERSION)
+})
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(process.env.PORT, function () {
+    console.log('Example app listening on port !' + process.env.PORT)
 })
